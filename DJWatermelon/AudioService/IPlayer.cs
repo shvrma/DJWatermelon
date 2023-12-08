e4be324013929e0250fdf341bacad7a8adbb6049
shvrma_ <145193519+shvrma@users.noreply.github.com>
2023-12-08 23:08:04 +0100

    API extended.

--- DJWatermelon/AudioService/IPlayer.cs
+++ DJWatermelon/AudioService/IPlayer.cs
@@ -3,9 +3,14 @@
 internal interface IPlayer : IDisposable, IAsyncDisposable
 {
     LinkedList<ITrackHandle> Queue { get; }
+
     LinkedListNode<ITrackHandle>? CurrentTrack { get; }
+
     Task PlayAsync(ITrackHandle track);
+
     Task PlayQueued();
+
     Task PlayNextAsync();
+
     Task PlayPreviousAsync();
 }
